6ed84d267129b4fe4b2ba363ea61e5e409d15a4c
Federico Giacon <58218759+fedgiac@users.noreply.github.com>
2022-01-18 10:25:57 +0100

    Verify contract on Etherscan

--- src/tasks/index.ts
+++ src/tasks/index.ts
@@ -1,5 +1,7 @@
 import { setupTestDeploymentTask } from "./test-deployment";
+import { setupVerifyContractCodeTask } from "./verify-contract-code";
 
 export function setupTasks(): void {
   setupTestDeploymentTask();
+  setupVerifyContractCodeTask();
 }
