60f89801f121caa8df7c2bf383631f111eca6285
Federico Giacon <58218759+fedgiac@users.noreply.github.com>
2022-01-23 11:14:27 +0100

    work

--- src/tasks/index.ts
+++ src/tasks/index.ts
@@ -1,8 +1,10 @@
+import { setupDeploymentProposal } from "./deployment-proposal";
 import { setupTestClaimsTask } from "./test-claims";
 import { setupTestDeploymentTask } from "./test-deployment";
 import { setupVerifyContractCodeTask } from "./verify-contract-code";
 
 export function setupTasks(): void {
+  setupDeploymentProposal();
   setupTestClaimsTask();
   setupTestDeploymentTask();
   setupVerifyContractCodeTask();
