8089ecd0946b8c4320358878a1610faf2cbefc77
Federico Giacon <58218759+fedgiac@users.noreply.github.com>
2022-01-25 17:08:56 +0000

    Add forwarder contract (#67)
    * Add forwarder contract
    
    * Fix lint

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,6 +1,7 @@
 export * from "./claim";
 export * from "./csv";
 export * from "./deploy";
+export * from "./forwarder";
 export * from "./permit";
 export * from "./proposal";
 export * from "./split";
