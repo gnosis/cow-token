3f20c9d5ce82f182976ce0b70354e58df17a3f01
Federico Giacon <58218759+fedgiac@users.noreply.github.com>
2022-02-24 19:58:08 +0100

    Remove unused variable

--- test/dummy-instantiation.ts
+++ test/dummy-instantiation.ts
@@ -1,9 +1,4 @@
-import { SafeCreationSettings, BridgeParameter } from "../src/ts";
-
-export const dummyteamControllerSettings: SafeCreationSettings = {
-  owners: [3].map((i) => "0x".padEnd(42, i.toString())),
-  threshold: 1,
-};
+import { BridgeParameter } from "../src/ts";
 
 export const dummyBridgeParameters: BridgeParameter = {
   multiTokenMediatorGnosisChain: "0x" + "01".repeat(20),
